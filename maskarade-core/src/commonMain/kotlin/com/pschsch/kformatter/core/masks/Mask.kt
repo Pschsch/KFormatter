804cd42e5112ddad55b263c6232714a756a6cb0c
MaxxxGordiyyy <maxxxgordiyyy@gmail.com>
2022-11-16 12:39:24 +0300

    v 0.0.1-alpha12: - Warnings fix

--- maskarade-core/src/commonMain/kotlin/com/pschsch/kformatter/core/masks/Mask.kt
+++ maskarade-core/src/commonMain/kotlin/com/pschsch/kformatter/core/masks/Mask.kt
@@ -2,16 +2,13 @@ package com.pschsch.kformatter.core.masks
 
 import com.pschsch.kformatter.core.formatting.MaskFormatter
 import com.pschsch.kformatter.core.formatting.builder.MaskFormatterBuilder
-import com.pschsch.kformatter.core.formatting.builder.MaskFormatterBuilderImpl
-import com.pschsch.kformatter.core.masks.builder.MaskBuilder
 import com.pschsch.kformatter.core.masks.internal.createFormatter
-import com.pschsch.kformatter.core.masks.internal.createMask
-import com.pschsch.kformatter.core.optin.IncubatingKFormatterAPI
+import com.pschsch.kformatter.core.optin.IncubatingMaskaradeAPI
 import com.pschsch.kformatter.core.parcelable.Parcelable
 import com.pschsch.kformatter.core.parcelable.Parcelize
 import com.pschsch.kformatter.core.serializable.Serializable
 
-@IncubatingKFormatterAPI
+@IncubatingMaskaradeAPI
 interface Mask : Collection<Mask.Slot>, Parcelable, Serializable {
 
     val hint : String
@@ -30,8 +27,8 @@ interface Mask : Collection<Mask.Slot>, Parcelable, Serializable {
     companion object
 }
 
-@IncubatingKFormatterAPI
+@IncubatingMaskaradeAPI
 fun Mask.formatter(block : MaskFormatterBuilder.() -> Unit = {}): MaskFormatter = createFormatter(listOf(this), block)
 
-@IncubatingKFormatterAPI
+@IncubatingMaskaradeAPI
 fun List<Mask>.formatter(block : MaskFormatterBuilder.() -> Unit = {}): MaskFormatter = createFormatter(this, block)
