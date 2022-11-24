c62efd78c20f678eeb18649169f3d9c595ab6f4e
MaxxxGordiyyy <maxxxgordiyyy@gmail.com>
2022-11-24 18:05:35 +0300

    Added tests

--- maskarade-core/src/commonMain/kotlin/com/pschsch/maskarade/core/implementation/mask/MaskFormatterImpl.kt
+++ maskarade-core/src/commonMain/kotlin/com/pschsch/maskarade/core/implementation/mask/MaskFormatterImpl.kt
@@ -1,8 +1,6 @@
 package com.pschsch.maskarade.core.implementation.mask
 
-import com.pschsch.maskarade.core.api.mask.Mask
 import com.pschsch.maskarade.core.api.mask.MaskFormatter
-import com.pschsch.maskarade.core.api.mask.MaskSlot
 import com.pschsch.maskarade.core.api.optin.IncubatingMaskaradeAPI
 
 @OptIn(IncubatingMaskaradeAPI::class)
@@ -10,6 +8,6 @@ internal class MaskFormatterImpl(
     charSequence: CharSequence
 ) : MaskFormatter {
 
-    override val mask: Mask = MaskImpl(MaskFormatParser.parse(charSequence))
+    override val mask = MaskImpl(MaskFormatParser.parse(charSequence))
 
 }
--- maskarade-core/src/commonTest/kotlin/com/pschsch/maskarade/test/MaskFormatterTest.kt
+++ maskarade-core/src/commonTest/kotlin/com/pschsch/maskarade/test/MaskFormatterTest.kt
@@ -4,6 +4,7 @@ import com.pschsch.maskarade.core.api.mask.factories.MaskFormatter
 import com.pschsch.maskarade.core.api.mask.matches
 import com.pschsch.maskarade.core.api.optin.IncubatingMaskaradeAPI
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
@@ -17,5 +18,26 @@ class MaskFormatterTest {
         assertTrue("01.15.2022".matches(mask))
         assertFalse("11.09.2021".matches(mask))
     }
+
+    @Test
+    fun testMaskFormatterValidation() {
+        fun exceptionMessage(sequence: CharSequence) = "Invalid sequence detected in provided format: $sequence"
+        val message1 = getValidationExceptionMessage("[0-3]D.[0-1]D.202[2-5]")
+        val message2 = getValidationExceptionMessage("[lS]?squisqui[SLC]?")
+        val message3 = getValidationExceptionMessage("[lS]?squisqui[LCS]!")
+        assertEquals(null, message1)
+        assertEquals(null, message2)
+        assertEquals(exceptionMessage("[LCS]!"), message3)
+    }
+
+    private fun getValidationExceptionMessage(sequence: CharSequence): String? {
+        return try {
+            MaskFormatter(sequence)
+            null
+        } catch (e: IllegalStateException) {
+            e.message
+        }
+    }
+
 
 }
